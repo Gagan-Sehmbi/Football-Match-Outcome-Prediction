2395acaddcc6b7a24f37a8d0b3b2050221d97995
Gagan Sehmbi <gagan.sehmbi@outlook.com>
2022-04-11 20:05:47 +0100

    Update to present clean results that are easy to understand

--- model_inference.py
+++ model_inference.py
@@ -47,40 +47,10 @@ data = (df.values - params['Mean'].values)/params['STD'].values
 results = classifier.predict(data)
 
 df['Prediction'] = results
+df['Prediction'] = df['Prediction'].apply(lambda x: classes.loc[x, '0'])
 
-# %%
+# %% 
+df['Prediction'].value_counts()
 
-df['Home_Team_Win_Streak'].value_counts()
 
-# %% IMPORT LIBRARIES
-import time
-
-import numpy as np
-import pandas as pd
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-import pymysql
-from sqlalchemy import create_engine, inspect
-from login import football_result_prediction_db_details
-
-from joblib import load
-from sklearn.metrics import classification_report, confusion_matrix
-
-classifier = load('classifier.joblib')
-
-df = pd.read_sql_table('Clean_Train_Dataset', engine, index_col=0)
-data = (df.drop(columns=['Labels']).values - params['Mean'].values)/params['STD'].values
-results = classifier.predict(data)
-df['Prediction'] = results
-
-print(confusion_matrix(y_true=df['Labels'].values, y_pred=df['Prediction']))
-print(classification_report(y_true=df['Labels'].values, y_pred=df['Prediction']))
-
-df
-# %%
-df['Home_Team_Win_Streak'].value_counts()
-# %%
-df.loc[df['Season'] ==2022, 'Home_Team_Win_Streak'].value_counts()
 # %%
